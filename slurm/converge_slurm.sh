b3ae657e1cc7db396d044b3e4bda282c5a839498
Maria J. Sanchez Rincon <mariajsanchez006@gmail.com>
2025-08-05 11:25:19 -0700

    Update converge_slurm.sh

--- slurm/converge_slurm.sh
+++ slurm/converge_slurm.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name="convergence test"
 #SBATCH --time=10:00:00
-#SBATCH --array=1-100
+##SBATCH --array=1-100
 #SBATCH --mem=3G
 #SBATCH --output=/carnegie/nobackup/scratch/msanchezrincon/slurm_output/chain/out-array_%A_%a.out  
 #SBATCH --error=/carnegie/nobackup/scratch/msanchezrincon/slurm_output/chain/err-array_%A_%a.err  
@@ -10,9 +10,9 @@
 #SBATCH --ntasks=1
 
 
-echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+##echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 
 module load python
 conda activate emcee_fitting
 
-python3 mcmc/scripts/clean_chains_O3.py $SLURM_ARRAY_TASK_ID
+python3 mcmc/scripts/clean_chains_O3.py ##$SLURM_ARRAY_TASK_ID
