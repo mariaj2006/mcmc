b53e1a87647259f6e032ff269d33782063c4db70
Maria J. Sanchez Rincon <mariajsanchez006@gmail.com>
2025-08-07 17:47:21 -0700

    Update vmap.py

--- scripts/vmap.py
+++ scripts/vmap.py
@@ -96,7 +96,11 @@ print(len(xx_val))
 # Monte Carlo sample of the velocity fields, using only one component fitting results
 t0 = time.time()
 for xi, yi in zip(xx_val, yy_val):
-    chain_file = path_prefix + 'chains_cleaned/vel_chains/cleaned_mc_%d_%d.fits' % (xi,yi)
+    try:
+        chain_file = path_prefix + 'chains_cleaned/vel_chains/cleaned_mc_%d_%d.fits' % (xi,yi)
+    except FileNotFoundError:
+        print(f"{xi} {yi} not found")
+        continue
     chains = fits.getdata(chain_file)
     redshifts = chains[:, -250:, 0].flatten() # take the last -250 samples from all chains
     vels = (redshifts - z0)/(1 + z0) * clight
